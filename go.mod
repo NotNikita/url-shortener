05df40ec99cde8f1cdbb5cf44eb61c70809f1604
NotNikita <nikitabsuir@gmail.com>
2024-11-22 00:05:46 +0100

    aws configured and post requst done

--- go.mod
+++ go.mod
@@ -5,9 +5,31 @@ go 1.23.3
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/aws/aws-sdk-go v1.55.5 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.32.5 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.28.5 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.46 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.15.17 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression v1.7.52 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.20 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.24 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.24 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.37.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.24.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.10.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.24.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.1 // indirect
+	github.com/aws/smithy-go v1.22.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/gofiber/fiber/v2 v2.52.5 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -1,41 +1,93 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"github.com/gofiber/fiber/v2"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/go-redis/redis/v8"
+	"net/url" // to validate urls
+	"time"    // to create time variables
 	"context"
+	"github.com/joho/godotenv"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	// "github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	// "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/gofiber/fiber/v2"
+	// "github.com/go-redis/redis/v8"
 	"go.uber.org/zap"
 )
 
-var db *dynamodb.DynamoDB
+// TableBasics encapsulates the Amazon DynamoDB service actions used in the examples.
+// It contains a DynamoDB service client that is used to act on the specified table.
+type TableBasics struct {
+	DynamoDbClient *dynamodb.Client
+	TableName      string
+}
+
+// Movie encapsulates data about a movie. Title and Year are the composite primary key
+// of the movie in Amazon DynamoDB. Title is the sort key, Year is the partition key,
+// and Info is additional data.
+type UrlData struct {
+	ShortUrl    string `dynamodbav:"shortCode"`
+	OriginalUrl string `dynamodbav:"originalURL"`
+	CreatedAt   string `dynamodbav:"createdAt"`
+	ExpiresAt   string `dynamodbav:"expiresAt"`
+}
+
 var logger *zap.Logger
+var basics TableBasics
 
 func init() {
-	// Our logger
 	var err error
+
+	// Load environment variables from .env
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Our logger
 	logger, err = zap.NewProduction()
 	if err != nil {
 		log.Fatalf("Can't initialize zap logger: %v", err)
 	}
 
-	// Our DynamoDB session
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("eu-central-1"),
-		Credentials: credentials.NewEnvCredentials(),
-	})
-	if err != nil {
-		log.Fatalf("Can't initialize db session: %v", err)
+	// Read AWS credentials and region from environment
+	awsAccessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	awsSecretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	awsRegion := os.Getenv("AWS_REGION")
+
+	if awsAccessKey == "" || awsSecretKey == "" || awsRegion == "" {
+		log.Fatalf("AWS credentials or region not set in .env file")
 	}
-	db = dynamodb.New(sess)
+
+	// Load AWS configuration with credentials provider
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(awsRegion),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(awsAccessKey, awsSecretKey, "")),
+	)
+	if err != nil {
+		logger.Fatal("Failed to load AWS config", zap.Error(err))
+	}
+
+	// Our DynamoDB session
+	dynamoClient := dynamodb.NewFromConfig(cfg)
+
+	// Set up TableBasics with the DynamoDB client and table name
+	basics = TableBasics{
+		DynamoDbClient: dynamoClient,
+		TableName:      "ShortUrls",
+	}
 }
 
-func main(){
+func main() {
 	app := fiber.New()
+	fmt.Println("Server started")
 
 	// Routes
 	app.Post("/shorten", shortenURL)
@@ -44,3 +96,103 @@ func main(){
 	// Start server
 	log.Fatal(app.Listen(":3000"))
 }
+
+func isValidUrl(urlToValidate string) bool {
+	u, err := url.Parse(urlToValidate)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+func shortenURL(c *fiber.Ctx) error {
+	form, err := c.MultipartForm()
+	if err != nil {
+		log.Fatalf("Error parsing provided data for shortenURL: %v", err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Failed to parse form: " + err.Error(),
+		})
+	}
+
+	long_url := form.Value["long_url"]
+	if len(long_url)== 0 || !isValidUrl(long_url[0]) {
+		log.Fatalf("Error when validating provided url to shorten")
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Failed to validate url: " + err.Error(),
+		})
+	}
+
+	now := time.Now().Format(time.RFC3339)
+	nowPlusMonth := time.Now().AddDate(0,1,0).Format(time.RFC3339)
+	// put valid in db
+	// TODO: short data xxHash creation
+	urlData := UrlData{
+		ShortUrl:    "short-" + time.Now().Format(time.RFC3339),
+		OriginalUrl: long_url[0],
+		ExpiresAt:   nowPlusMonth,
+		CreatedAt:   now,
+	}
+	fmt.Println("urlData", urlData)
+
+	preparedData, err := attributevalue.MarshalMap(urlData)
+	fmt.Println("preparedData", preparedData)
+
+	if err != nil {
+		log.Fatalf("Failed to prepare data for database")
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to prepare data for database: " + err.Error(),
+		})
+	}
+
+	_, err = basics.DynamoDbClient.PutItem(c.Context(), &dynamodb.PutItemInput{
+		TableName: aws.String(basics.TableName),
+		Item:      preparedData,
+	})
+	if err != nil {
+		log.Fatalf("Failed to add item to ShortUrls table: %v\nURL Data: %+v\nPrepared Data: %+v", err, urlData, preparedData)
+
+		// Handle specific error types if needed
+		if awsErr, ok := err.(awserr.Error); ok {
+			switch awsErr.Code() {
+			case "ProvisionedThroughputExceededException":
+				log.Fatalf("Provisioned throughput exceeded for table: %s", basics.TableName)
+				// Implement backoff or rate limiting strategy
+			case "AccessDeniedException":
+				log.Fatalf("Access denied for table: %s. Check IAM permissions.", basics.TableName)
+			default:
+				log.Fatalf("Unknown error: %v", awsErr)
+			}
+		}
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to add item to ShortUrls table: " + err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"shortenURL": urlData.ShortUrl,
+	})
+}
+
+func getURL(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"getURL": "hello",
+	})
+}
+
+// // GetKey returns the composite primary key of the movie in a format that can be
+// // sent to DynamoDB.
+// func (movie Movie) GetKey() map[string]types.AttributeValue {
+// 	title, err := attributevalue.Marshal(movie.Title)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	year, err := attributevalue.Marshal(movie.Year)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return map[string]types.AttributeValue{"title": title, "year": year}
+// }
+
+// // String returns the title, year, rating, and plot of a movie, formatted for the example.
+// func (movie Movie) String() string {
+// 	return fmt.Sprintf("%v\n\tReleased: %v\n\tRating: %v\n\tPlot: %v\n",
+// 		movie.Title, movie.Year, movie.Info["rating"], movie.Info["plot"])
+// }
